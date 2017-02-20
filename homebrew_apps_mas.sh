55126300a769811884a4ee34efd50751dbfe1759
Daniel Hellfeld <Hellfeld@Daniels-MacBook-Pro.local>
2017-02-20 09:55:40 -0800

    Mas install script for previous purchased Apple App Store apps

---
+++ homebrew_apps_mas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash                                                                                
+# Memory clean 2
+mas install 1114591412
+
+# Disk doctor
+mas install 455970963
+
+# Cinch
+mas install 412529613
+
+# Battery health
+mas install 490192174
