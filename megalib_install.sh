ec67fb365ef3b44815d474d880dd95b1edd4ef84
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-02-25 08:48:10 -0800

    Add git user option

--- megalib_install.sh
+++ megalib_install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Which git user's repo
+export gituser="dhellfeld"                            # ["dhellfeld" or "zoglauer"]
+
 # Build options
-export RELEASE="dev"                                       # [rel or dev]
-export PATCH="yes"                                         # [yes or no]
-export CLEANUP="yes"                                       # [yes or no]
-export MAXTHREADS="4"                                      # [1,2,3,...]
-export GEANT_PATH=~/GEANT4.10.2.p1/geant4-install/         # [/path/to/ or ""]
-export ROOT_PATH=~/ROOT_v6.08.04/root-install/             # [/path/to/ or ""]
-export MEGdir=$PWD/MEGAlib                                 # Path to install directory
+export RELEASE="dev"                                  # [rel or dev]
+export PATCH="yes"                                    # [yes or no]
+export CLEANUP="yes"                                  # [yes or no]
+export MAXTHREADS="4"                                 # [1,2,3,...]
+export GEANT_PATH=~/GEANT4.10.2.p1/geant4-install/    # [/path/to/ or ""]
+export ROOT_PATH=~/ROOT_v6.08.04/root-install/        # [/path/to/ or ""]
+export MEGdir=$PWD/MEGAlib                            # Path to install directory
 
 echo "Cloning into repo..."
-git clone https://github.com/zoglauer/megalib $MEGdir
+git clone https://github.com/$gituser/megalib $MEGdir
 cd $MEGdir
 
 echo "Running the setup script..."
