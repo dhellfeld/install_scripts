105284a8092ce2d303b02a88860c51dca4a6e38a
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-02-24 17:42:56 -0800

    MEGAlib install script, pointing to GEANT and ROOT installs, GEANT must be compiled without QT...

---
+++ megalib_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Build options
+export RELEASE="dev"                                       # [rel or dev]
+export PATCH="yes"                                         # [yes or no]
+export CLEANUP="yes"                                       # [yes or no]
+export MAXTHREADS="4"                                      # [1,2,3,...]
+export GEANT_PATH=~/GEANT4.10.2.p1/geant4-install/         # [/path/to/ or ""]
+export ROOT_PATH=~/ROOT_v6.08.04/root-install/             # [/path/to/ or ""]
+export MEGdir=$PWD/MEGAlib                                 # Path to install directory
+
+echo "Cloning into repo..."
+git clone https://github.com/zoglauer/megalib $MEGdir
+cd $MEGdir
+
+echo "Running the setup script..."
+./setup.sh --release=$RELEASE --root=$ROOT_PATH --geant=$GEANT_PATH --maxthreads=$MAXTHREADS --patch=$PATCH --cleanup=$CLEANUP
+
+
+echo "Done!"
+
+# Print bashrc advice to screen (user can decide to use or not...)
+echo "\nAdd the following to your .bashrc or .bash_profile:\n"
+echo "#--------------------------------"
+echo "#  MEGAlib\n"
+echo ". $MEGdir/bin/source-megalib.sh"
+echo "# --------------------------------\n"
