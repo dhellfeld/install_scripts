a3f17b2923b0a20e251fed25e9992ed36a42a8cf
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-02-25 09:33:20 -0800

    homebrew/versions

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -3,6 +3,9 @@
 echo "Installing homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Tapping homebrew/versions..."
+brew tap homebrew/versions   # can (sometimes) install various versions of tools
+
 echo "Installing cask (application installer)..."
 brew tap 'caskroom/cask'
 brew tap caskroom/versions   # can install various versions of apps
