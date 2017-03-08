27fd72fcb6ed662955e959497240670d7b4de838
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-03-08 11:18:07 -0800

    Include microsoft office 2016, add some office reorganization commands

--- homebrew_apps_cask.sh
+++ homebrew_apps_cask.sh
@@ -21,6 +21,7 @@ applications=(
     nvalt
     textwrangler
     microsoft-office-2011
+    microsoft-office
     doxygen
     ubersicht
     spectacle
@@ -30,8 +31,22 @@ applications=(
 echo "Installing applications..."
 brew cask install --appdir="/Applications" ${applications[@]}
 
+# --
+# Still deciding whether I want to use 2011 and 2016 office, install both for now
 
-## List of applications that must be installed by hand
+# Reorganize Microsoft 2016 install. Delete Outlook and OneNote
+cd /Applications
+mkdir Microsoft\ Office\ 2016
+mv Microsoft\ Word.app Microsoft\ PowerPoint.app Microsoft\ Excel.app Microsoft\ Office\ 2016
+rm -r Microsoft\ Outlook.app Microsoft\ OneNote.app
+cd ~/
+
+# Delete Microsoft Office 2011 Outlook
+rm -rf /Applications/Microsoft\ Office\ 2011/Microsoft\ Outlook.app
+
+
+# --
+# List of applications that must be installed by hand
 # Mathemtica
 # Matlab
 # DivX player
