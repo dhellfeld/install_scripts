4feee4b65fba750141ac82c1afab1dd09f393f75
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-06-17 10:37:57 -0600

    Add joblib and progressbar

--- anaconda2_packages.sh
+++ anaconda2_packages.sh
@@ -14,3 +14,9 @@ conda install -c conda-forge pyopencl=2016.2.1
 
 # OpenCV 3
 conda install --channel https://conda.anaconda.org/menpo opencv3
+
+# Joblib (SVM trainer sklearn)
+conda install joblib
+
+# Progressbar
+conda install progressbar
