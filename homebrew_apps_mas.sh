f20ab3c3a285d4ef9501783de056d830ea0edccf
Daniel Hellfeld <dhellfeld@berkeley.edu>
2017-06-18 08:45:14 -0700

    Update batter health (mas)

--- homebrew_apps_mas.sh
+++ homebrew_apps_mas.sh
@@ -9,8 +9,11 @@ mas install 455970963
 # Cinch
 mas install 412529613
 
-# Battery health
-mas install 490192174
+# # Battery health
+# mas install 490192174
+
+# Batter health 2
+mas install 1120214373
 
 # Menu bar stats
 mas install 714196447
