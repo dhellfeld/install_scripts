500c5165ed214daf6986af92d3391acc651bb2cd
Daniel Hellfeld <dhellfeld@berkeley.edu>
2018-04-07 21:35:35 -0700

    Remove cinch, use spectacle now

--- MacOS/homebrew_apps_mas.sh
+++ MacOS/homebrew_apps_mas.sh
@@ -6,12 +6,6 @@ mas install 1114591412
 # Disk doctor
 mas install 455970963
 
-# Cinch
-mas install 412529613
-
-# # Battery health
-# mas install 490192174
-
 # Batter health 2
 mas install 1120214373
 
